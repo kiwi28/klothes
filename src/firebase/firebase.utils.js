7f8ebd0c3056adc10728b7804f548199193084fa
kiwi28 <alexkiwi.2011@gmail.com>
2020-09-23 17:18:39 +0300

    implemented firebase utils, included abilty to store authenticated users in db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
   appId: "1:80880318394:web:c2f6bb853e8d21c7960f83",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log("error craeting user", err.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/singInAndSignUp/singInAndSignUp.component.jsx
+++ src/pages/singInAndSignUp/singInAndSignUp.component.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import "./singInAndSignUp.styles.scss";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SingInAndSignUp = () => (
   <div className="singInAndSignUp">
     <SignIn />
+    <SignUp />
   </div>
 );
 
